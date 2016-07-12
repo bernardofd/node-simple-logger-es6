0b35273d842323bc06cf3d74e681a99064aafe2c
Bernardo Figuerêdo Domingues <bernardofd@gmail.com>
2016-07-12 16:30:56 -0300

    Original version

---
+++ index.js
@@ -0,0 +1,31 @@
+const
+	chalk  = require('chalk'),
+	moment = require('moment');
+
+// Log Levels
+const level = {
+	debug : chalk.green,
+	log : chalk.gray,
+	info : chalk.blue,
+	warn : chalk.yellow.bold,
+	error : chalk.bgRed.white.bold
+};
+
+// Timestamp format
+const timeFormat = 'DD-MMM-YYYY HH:mm:ss';
+
+function logFactory(lvl) {
+	return function(message) {
+		console.log(level[lvl](`[${moment().format(timeFormat)}] ${message}`));
+	};
+}
+
+module.exports = new Proxy({}, {
+	get : function(target, name) {
+		if (name in level) {
+			return logFactory(name);
+		} else {
+			return logFactory('log');
+		}
+	}
+});
